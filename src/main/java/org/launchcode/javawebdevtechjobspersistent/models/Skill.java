3c804740c4c45725ec03214d983c09012b80c4d0
Sean Small <seanj.small@live.com>
2020-10-27 01:55:11 -0500

    Parts 1 + 2 finished

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,28 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank(message = "Please enter a valid description")
+    private String description;
+
+    public Skill(@NotBlank(message = "Please enter a valid description") String description) {
+        this.description = description;
+    }
+
+    public Skill () {
+
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
